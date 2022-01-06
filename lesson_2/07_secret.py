b3d6fe5d7fa0107c035a211eed647d796553c29e
maximlarin <Maximlarin@mail.ru>
2022-01-06 14:55:46 +0300

    done_07

--- lesson_2/07_secret.py
+++ lesson_2/07_secret.py
@@ -21,14 +21,14 @@ secret_message = [
 # Требуется задать конкретные индексы, например secret_message[3][12:23:4]
 # Если нужны вычисления и разные пробы - делайте это в консоли пайтона, тут нужен только результат
 
-secret_message[0][3]
-secret_message[1][9:13]
-secret_message[2][5:15:2]
-secret_message[3][12:6:-1]
-secret_message[4][20:15:-1]
+# secret_message[0][3]
+# secret_message[1][9:13]
+# secret_message[2][5:15:2]
+# secret_message[3][12:6:-1]
+# secret_message[4][20:15:-1]
 
-print(secret_message[0][3],
+print(secret_message[0][3].upper(),
       secret_message[1][9:13],
       secret_message[2][5:15:2],
       secret_message[3][12:6:-1],
-      secret_message[4][20:15:-1])
+      secret_message[4][20:15:-1],)
