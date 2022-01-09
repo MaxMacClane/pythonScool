073a1b91c73ea44bf6fb61be9992f7b32a8d59f7
maximlarin <Maximlarin@mail.ru>
2022-01-09 16:07:33 +0300

    done store homework

--- lesson_03/05_store.py
+++ lesson_03/05_store.py
@@ -46,7 +46,46 @@ store = {
 #         подсчет стоимости товара
 #     вывод на консоль количества и стоимости товара на складе
 
-# TODO здесь ваш код
+for _ in goods.keys():
+    # in the store dictionary take the value quantity and the price, calculate total cost for each keys
+    # displaying value quantity, price and total cost
+    if _ == 'Лампа':
+        number_lamps = store[goods[_]][0]['quantity'], 'pcs'
+        cost_lamp = store[goods[_]][0]['price'], '$'
+        total_cost_lamps = number_lamps[0] * cost_lamp[0], '$'
+        print(_, '-', *number_lamps, 'for -', *cost_lamp)
+        print('Total cost lamps -', *total_cost_lamps)
+    if _ == 'Стол':
+        number_tables = store[goods[_]][0]['quantity'], 'pcs'
+        number_tables_one = store[goods[_]][1]['quantity'], 'pcs'
+        total_number_tables = number_tables[0] + number_tables_one[0], 'pcs'
+        cost_table = store[goods[_]][0]['price'], '$'
+        cost_table_one = store[goods[_]][1]['price'], '$'
+        total_cost_tables = (number_tables[0] * cost_table[0]) + (number_tables_one[0] * cost_table_one[0]), '$'
+        print(_, '-', *number_tables, 'for -', *cost_table, 'and', *number_tables_one, 'for', *cost_table_one)
+        print('Total cost tables -', *total_cost_tables)
+    if _ == 'Диван':
+        number_sofas = store[goods[_]][0]['quantity'], 'pcs'
+        number_sofas_one = store[goods[_]][1]['quantity'], 'pcs'
+        total_number_sofas = number_sofas[0] + number_sofas_one[0], 'pcs'
+        cost_sofa = store[goods[_]][0]['price'], '$'
+        cost_sofa_one = store[goods[_]][1]['price'], '$'
+        total_cost_sofas = (number_sofas[0] * cost_sofa[0]) + (number_sofas_one[0] * cost_sofa_one[0]), '$'
+        print(_, '-', *number_sofas, 'for -', *cost_sofa, 'and', *number_sofas_one, 'for', *cost_sofa_one)
+        print('Total cost sofas -', *total_cost_sofas)
+    if _ == 'Стул':
+        number_chairs = store[goods[_]][0]['quantity'], 'pcs'
+        number_chairs_one = store[goods[_]][1]['quantity'], 'pcs'
+        number_chairs_two = store[goods[_]][2]['quantity'], 'pcs'
+        total_number_chairs = number_chairs[0] + number_chairs_one[0], 'pcs'
+        cost_chair = store[goods[_]][0]['price'], '$'
+        cost_chair_one = store[goods[_]][1]['price'], '$'
+        cost_chair_two = store[goods[_]][2]['price'], '$'
+        total_cost_chairs = (number_chairs[0] * cost_chair[0]) + (number_chairs_one[0] * cost_chair_one[0]) + \
+                            (number_chairs_two[0] * cost_chair_two[0]), '$'
+        print(_, '-', *number_chairs, 'for -', *cost_chair, 'and', *number_chairs_one, 'for', *cost_chair_one,
+              'and', *number_chairs_two, 'for', *cost_chair_two)
+        print('Total cost chairs -', *total_cost_chairs)
 
 
 
