b636baf25edc83501c48059237276efbe943d7c3
maximlarin <Maximlarin@mail.ru>
2022-01-09 16:04:17 +0300

    done student homework

--- lesson_03/04_student.py
+++ lesson_03/04_student.py
@@ -8,7 +8,18 @@
 # чтобы можно было прожить учебный год (10 месяцев), используя только эти деньги и стипендию.
 # Формат вывода:
 #   Студенту надо попросить ХХХ.ХХ рублей
-
+quantity_months = int(input('Inter quantity months: '))
+inflation = 3
+money_from_parents = 0
 educational_grant, expenses = 10000, 12000
 
-# TODO здесь ваш код
+# for _ in range(quantity_months): 'if use loop
+while quantity_months > 0:
+    money_from_parents += expenses - educational_grant
+    expenses += (expenses * inflation) / 100
+    quantity_months -= 1
+print('For colledg living', quantity_months, 'need to ask your perents', money_from_parents.__round__(2))
+
+
+
+
