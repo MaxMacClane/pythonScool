95f489e24a2fba1c728c40b42b7cde44d1fe2c4e
maximlarin <Maximlarin@mail.ru>
2022-01-09 22:52:22 +0300

    done wall homework

--- lesson_03/07_wall.py
+++ lesson_03/07_wall.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
+
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
+x, y = 0, 0
+x1, y1 = 0, 50
+for y in range(0, 700, 50):
+    y1 = y + 50
+    r = (y / 50) % 2
+    if r == 1:
+        x, x1 = -50, 50
+    if r == 0:
+        x, x1 = 0, 100
+    for x in range(x, 700, 100):
+
+        left = sd.get_point(x, y)
+        right = sd.get_point(x1, y1)
+        x1 += 100
+        sd.rectangle(left, right, color=sd.COLOR_YELLOW, width=3)
 
 
-
-simple_draw.pause()
+sd.pause()
