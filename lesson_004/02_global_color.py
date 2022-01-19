19a32fff673df49480a73c3c9662360893ed1069
maximlarin <Maximlarin@mail.ru>
2022-01-19 21:59:30 +0300

    done  homework

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -13,7 +13,55 @@ import simple_draw as sd
 # sd.get_vector()
 # и константы COLOR_RED, COLOR_ORANGE, COLOR_YELLOW, COLOR_GREEN, COLOR_CYAN, COLOR_BLUE, COLOR_PURPLE
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
+list_figures = ["Threeangle", "Square", "Pentagon", "Hexagon"]
+user_color_list = []
+# loop iterates through the list_figures, prints a tooltip to the monitor, and writes the user's choice to
+# user_color_list
+for select_color in list_figures:
+    print('\nSELECT COLOR:', select_color, '\n 0 : RED \n 1 : BLACK\n 2 : WHITE\n 3 : YELLOW\n 4 : PURPLE\n'
+              ' 5 : ORANGE\n 6 : BLUE\n 7 : CYAN\n 8 : GREEN\n 9 : DARK YELLOW \n')
+    user_color = int( input('Inter namber '))
+    user_color_list.append(user_color)
 
-# TODO здесь ваш код
+list_color = [sd.COLOR_RED, sd.COLOR_BLACK, sd.COLOR_WHITE, sd.COLOR_YELLOW, sd.COLOR_PURPLE, sd.COLOR_ORANGE,
+              sd.COLOR_BLUE, sd.COLOR_CYAN, sd.COLOR_GREEN, sd.COLOR_DARK_YELLOW]
+
+
+def simple_figures(start_p, angle, length, width, color):
+    # the loop defines condihions and sets the parameters for changing the angle and start point for the vector func.
+    # end end_point for the funchion line
+    for r in range(4):
+        if r == 0:
+            step_angle = 120
+            start_p = sd.get_point(x, y)
+            end_point = start_p
+            color = list_color[user_color_list[0]]
+        if r == 1:
+            step_angle = 90
+            start_p = sd.get_point(x + 300, y)
+            end_point = start_p
+            color = list_color[user_color_list[1]]
+        if r == 2:
+            step_angle = 72
+            start_p = sd.get_point(x + 300, y + 250)
+            end_point = start_p
+            color = list_color[user_color_list[2]]
+        if r == 3:
+            step_angle = 60
+            start_p = sd.get_point(x, y + 250)
+            end_point = start_p
+            color = list_color[user_color_list[3]]
+        for angle in range(0, 360 - step_angle, step_angle):
+            vector = sd.get_vector(start_point=start_p, length=length, angle=angle, width=width)
+            vector.draw(color=color)
+            start_p = vector.end_point
+        start = vector.end_point
+        sd.line(start, end_point, width=width, color=color)
+
+
+x, y = 100, 50
+start_po = sd.get_point(x, y)
+_angle = 1
+simple_figures(start_p=start_po, angle=_angle, length=150, width=3, color=list_color)
 
 sd.pause()
