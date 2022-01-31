8a9c9107d8ef7bb8feb9f05c4609e889da22b7f1
maximlarin <Maximlarin@mail.ru>
2022-01-31 20:53:12 +0300

    done  homework lesson 2 and 3

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -15,19 +15,31 @@ import simple_draw as sd
 # Результат решения см lesson_004/results/exercise_02_global_color.jpg
 list_figures = ["Threeangle", "Square", "Pentagon", "Hexagon"]
 user_color_list = []
-# loop iterates through the list_figures, prints a tooltip to the monitor, and writes the user's choice to
-# user_color_list
-for select_color in list_figures:
-    print('\nSELECT COLOR:', select_color, '\n 0 : RED \n 1 : BLACK\n 2 : WHITE\n 3 : YELLOW\n 4 : PURPLE\n'
-              ' 5 : ORANGE\n 6 : BLUE\n 7 : CYAN\n 8 : GREEN\n 9 : DARK YELLOW \n')
-    user_color = int( input('Inter namber '))
-    user_color_list.append(user_color)
+def try_again():
+    # loop iterates through the list_figures, prints a tooltip to the monitor, and writes the user's choice to
+    # user_color_list
+    for select_color in list_figures:
+        print('\nSELECT COLOR:', select_color, '\n 0 : RED \n 1 : BLACK\n 2 : WHITE\n 3 : YELLOW\n 4 : PURPLE\n'
+                  ' 5 : ORANGE\n 6 : BLUE\n 7 : CYAN\n 8 : GREEN\n 9 : DARK YELLOW \n')
+        user_color = int( input('Inter namber '))
+
+        while user_color > 9:
+            print('Inpyt namber from 0 to 9. Start over)'.upper())
+            print('\nSELECT COLOR:', select_color, '\n 0 : RED \n 1 : BLACK\n 2 : WHITE\n 3 : YELLOW\n 4 : PURPLE\n'
+                                                   ' 5 : ORANGE\n 6 : BLUE\n 7 : CYAN\n 8 : GREEN\n 9 : DARK YELLOW \n')
+            user_color = int(input('Inter namber '))
+        user_color_list.append(user_color)
+        if select_color == "Hexagon":
+            break
+
+
+try_again()
 
 list_color = [sd.COLOR_RED, sd.COLOR_BLACK, sd.COLOR_WHITE, sd.COLOR_YELLOW, sd.COLOR_PURPLE, sd.COLOR_ORANGE,
               sd.COLOR_BLUE, sd.COLOR_CYAN, sd.COLOR_GREEN, sd.COLOR_DARK_YELLOW]
 
 
-def simple_figures(start_p, angle, length, width, color):
+def simple_figures(start_p, angle, length, width):
     # the loop defines condihions and sets the parameters for changing the angle and start point for the vector func.
     # end end_point for the funchion line
     for r in range(4):
@@ -62,6 +74,6 @@ def simple_figures(start_p, angle, length, width, color):
 x, y = 100, 50
 start_po = sd.get_point(x, y)
 _angle = 1
-simple_figures(start_p=start_po, angle=_angle, length=150, width=3, color=list_color)
+simple_figures(start_p=start_po, angle=_angle, length=150, width=3)
 
 sd.pause()
--- lesson_004/03_shape_select.py
+++ lesson_004/03_shape_select.py
@@ -9,6 +9,74 @@ import simple_draw as sd
 # Код функций из упр lesson_004/02_global_color.py скопировать сюда
 # Результат решения см lesson_004/results/exercise_03_shape_select.jpg
 
-# TODO здесь ваш код
+list_figures = ["Threeangle", "Square", "Pentagon", "Hexagon"]
+
+list_color = [sd.COLOR_RED, sd.COLOR_BLACK, sd.COLOR_WHITE, sd.COLOR_YELLOW, sd.COLOR_PURPLE, sd.COLOR_ORANGE,
+              sd.COLOR_BLUE, sd.COLOR_CYAN, sd.COLOR_GREEN, sd.COLOR_DARK_YELLOW]
+
+
+def simple_figures(start_p, angle, length, width, color):
+    # the loop defines condihions and sets the parameters for changing the angle and start point for the vector func.
+    # end end_point for the funchion line
+    print('\nSELECT FIGUR: \n 0 : Threeangle \n 1 : Square \n 2 : Pentagon \n 3 : Hexagon')
+    user_figur = int(input('Inter namber '))
+
+    while user_figur > 3:
+        print('\nInput namber from 0 to 3. Start over'.upper())
+        print('\nSELECT FIGUR: \n 0 : Threeangle \n 1 : Square \n 2 : Pentagon \n 3 : Hexagon')
+        user_figur = int(input('Inter namber '))
+
+
+    print('\nSELECT COLOR:', list_figures[user_figur], '\n 0 : RED \n 1 : BLACK\n 2 : WHITE\n 3 : YELLOW\n 4 : PURPLE\n'
+                                                       ' 5 : ORANGE\n 6 : BLUE\n 7 : CYAN\n 8 : GREEN\n 9 : DARK YELLOW \n')
+    user_color = int(input('Inter namber '))
+
+
+    while user_color > 9:
+        print('\nInpyt namber from 0 to 9. Start over)'.upper())
+        print('\nSELECT COLOR:', list_figures[user_figur],
+              '\n 0 : RED \n 1 : BLACK\n 2 : WHITE\n 3 : YELLOW\n 4 : PURPLE\n'
+              ' 5 : ORANGE\n 6 : BLUE\n 7 : CYAN\n 8 : GREEN\n 9 : DARK YELLOW \n')
+        user_color = int(input('Inter namber '))
+
+
+
+    if user_figur == 0:
+        step_angle = 120
+        start_p = sd.get_point(x, y)
+        end_point = start_p
+        color = list_color[user_color]
+
+    if user_figur == 1:
+        step_angle = 90
+        start_p = sd.get_point(x, y)
+        end_point = start_p
+        color = list_color[user_color]
+
+    if user_figur == 2:
+        step_angle = 72
+        start_p = sd.get_point(x, y)
+        end_point = start_p
+        color = list_color[user_color]
+
+    if user_figur == 3:
+        step_angle = 60
+        start_p = sd.get_point(x, y)
+        end_point = start_p
+        color = list_color[user_color]
+
+    for angle in range(0, 360 - step_angle, step_angle):
+        vector = sd.get_vector(start_point=start_p, length=length, angle=angle, width=width)
+        vector.draw(color=color)
+        start_p = vector.end_point
+    start = vector.end_point
+    sd.line(start, end_point, width=width, color=color)
+
+
+x, y = 200, 250
+start_po = sd.get_point(x, y)
+_angle = 1
+
+simple_figures(start_p=start_po, angle=_angle, length=150, width=3, color=list_color)
 
 sd.pause()
