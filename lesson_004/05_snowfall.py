7cc720e2a6ebdba60cae184171fcd7faf9202330
maximlarin <Maximlarin@mail.ru>
2022-02-05 21:18:07 +0300

    homework still don't don day 2

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -9,7 +9,6 @@ sd.resolution = 600, 600
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
 
-N = 20
 
 # Пригодятся функции
 # sd.get_point()
@@ -17,27 +16,48 @@ N = 20
 # sd.sleep()
 # sd.random_number()
 # sd.user_want_exit()
-x, y = 220, 500
+N = 5
+x, y = 0, 0
 
 
-def random_valeu(point, namber, condition=False):
+def random_start(condition=False):
+    x = sd.random_number(0, 600)
+    y = sd.random_number(550, 600)
     if condition:
-        point_x = sd.random_number(point, namber)
-        return point_x
+        return x
+    else:
+        return y
 
 
-quantity = 0
-point = sd.get_point(random_valeu(0, 600), y)
+def snowf_value():
+    length_list = []
+    return {'length': length_list,
+            'x': random_start(condition=True),
+            'y': random_start(condition=False)
+            }
+
+
+snowf_lists = []
+
+for _ in range(N):
+    snowf_lists.append(snowf_value())
+    for length in range(1, sd.random_number(20, 30), sd.random_number(2, 30)):
+        # for length in range(sd.random_number(0, 600)):
+        snowf_lists[_]['length'].append(length)
+
+    print(snowf_lists)
+
 while True:
     sd.clear_screen()
-    point.x += sd.random_number(-30, 30)
-    point.y -= sd.random_number(0, 30)
 
-    for length in range(3, sd.random_number(10, 30), sd.random_number(2, 5)):
-    # for length in range(sd.random_number(0, 600)):
-        sd.snowflake(center=point, length=length, color=sd.COLOR_WHITE)
-    if point.y < 10:
-        point.y = 600 - sd.random_number(0, 30)
+    for flake in snowf_lists:
+        x = snowf_lists[0]['x']
+        y = snowf_lists[0]['y']
+        # for length in snowf_lists[0]['length'][4]
+        length = snowf_lists[0]['length'][1]
+        sd.snowflake(center=sd.get_point(x, y), length=length, color=sd.COLOR_WHITE)
+        print(sd.get_point(x,y))
+
     sd.sleep(0.6)
 
     if sd.user_want_exit():
