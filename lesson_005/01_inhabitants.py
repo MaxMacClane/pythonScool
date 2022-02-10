9b2c09484d97250ec4f6b4e2ffd0a76184e99460
maximlarin <Maximlarin@mail.ru>
2022-02-10 21:02:30 +0300

    done homework

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -5,6 +5,6 @@ import room_2
 # Формат: В комнате room_1 живут: ...
 
 print('In the room room_1 live: ', *room_1.folks)
-print('In the room room_1 live: ', *room_2.folks)
+print('In the room room_2 live: ', *room_2.folks)
 
 
