a18c91d7a8b8d6764a043c9cddb34ccba621743c
maximlarin <Maximlarin@mail.ru>
2022-02-10 20:55:20 +0300

    done homework

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-
+import room_1
+import room_2
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+print('In the room room_1 live: ', *room_1.folks)
+print('In the room room_1 live: ', *room_2.folks)
 
 
