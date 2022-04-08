2e382b759f3d8a6d381e2d2cf7406862bf5c7b08
maximlarin <Maximlarin@mail.ru>
2022-04-08 18:53:55 +0300

    made package with modules for draw village house

--- lesson_005/test.py
+++ lesson_005/test.py
@@ -6,12 +6,13 @@ vi.ground()
 st_point = sd.get_point(1600, 100)
 _length = 175
 _angle = 90
-vi.draw_branches(s_point=st_point, length=_length, angle=_angle)
 
 vi.rainbow()
 vi.smale_mane()
 vi.sun()
+vi.draw_branches(s_point=st_point, length=_length, angle=_angle)
 vi.wall()
+
 vi.cloud()
 vi.snowfall()
 
