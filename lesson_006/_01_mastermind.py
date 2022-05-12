80872fa5635eb1aea32fc3399cd23318d9053b0a
maximlarin <Maximlarin@mail.ru>
2022-05-12 21:55:44 +0300

    game bulls and cows added color to terminal / third day

--- lesson_006/_01_mastermind.py
+++ lesson_006/_01_mastermind.py
@@ -43,32 +43,33 @@
 # Это пример применения SOLID принципа (см https://goo.gl/GFMoaI) в архитектуре программ.
 # Точнее, в этом случае важен принцип единственной ответственности - https://goo.gl/rYb3hT
 
-from masrermind_engine import set_number, check_number
+from mastermind_engine import set_number, check_number
 from termcolor import cprint
 
 
 
 
-def bulls():
-    reference = set_number()
-    move_count = 0
-    cprint("Try to guess the number puzzled by the computer", color='magenta')
-    res = 0
-    while res != reference:
-        res = input("Enter a four-digit number")
-        print('*' * 44)
-        move_count += 1
-        check_number(represented_number=res, reference=reference)
-        if res == reference:
-            cprint(" You win! Guessed number is {}".format(reference), 'green', attrs=['underline'])
-            cprint("Move count {}".format(move_count), 'green', attrs=['underline'])
-            print()
-            question = input("If you want to play more, enter yes, otherwise no ")
-            if question == "yes" or question == "YES" or question == "Yes":
-                reference = 0
-            else:
-                cprint("   GAME OVER   ", 'green', 'on_grey', ['reverse'])
+from mastermind_engine import set_number, check_number
+from termcolor import cprint
+represented_number = 0
+reference = set_number()
+move_count = 0
+cprint("Try to guess the number puzzled by the computer", color='magenta')
+while represented_number != reference:
+    represented_number = input("Enter a four-digit number ")
+    print('*' * 44)
+    move_count += 1
+    check_number(represented_number=represented_number, reference=reference)
+    if represented_number == reference:
+        cprint(" You win! Guessed number is {}".format(reference), 'green', attrs=['underline'])
+        cprint("Move count {}".format(move_count), 'green', attrs=['underline'])
+        print()
+        question = input("If you want to play more, enter yes, otherwise no ")
+        if question == "yes" or question == "YES" or question == "Yes":
+            reference = 0
+        else:
+            cprint("   GAME OVER   ", 'green', 'on_grey', ['reverse'])
 
 
 
-bulls()
+
