a441aefe373611ec6e6e49e125a77b1bdc778199
maximlarin <Maximlarin@mail.ru>
2022-05-12 21:49:50 +0300

    game bulls and cows added color to terminal / third day

---
+++ lesson_006/python_snippets/masrermind_engine.py
@@ -0,0 +1,23 @@
+from random import randint, random
+from termcolor import cprint
+
+_guess_number = 0
+
+
+def set_number():
+    global _guess_number
+    _guess_number = str(randint(1000, 9000))
+    return _guess_number
+
+
+def check_number(represented_number, reference):
+    bulls = 0
+    cows = 0
+    for indexp, ip in enumerate(represented_number):
+        for indexr, ir in enumerate(reference):
+            if ip == ir and indexp == indexr:
+                bulls += 1
+                break
+            elif ip == ir and ip != reference[indexp]:
+                cows += 1
+    return cprint("bulls {}, cows {}".format(bulls, cows, ), color='red')
