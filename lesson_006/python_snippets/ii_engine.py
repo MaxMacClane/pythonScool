8fc40d9b3500f2a97a01852963151e9e70933b46
maximlarin <Maximlarin@mail.ru>
2022-05-13 20:21:23 +0300

    game bulls and cows added color to terminal / fourth day

---
+++ lesson_006/python_snippets/ii_engine.py
@@ -0,0 +1,19 @@
+
+
+
+def der():
+    f = input("Enter a four-digit number ")
+    return f
+
+
+def fix_import():
+    from ._01_mastermined import check_n
+    f = check_n()
+    print(f)
+
+
+fix_import()
+
+
+
+
