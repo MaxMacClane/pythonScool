597c56674d92a0ba399742c1ecdcc45a1a229f01
maximlarin <Maximlarin@mail.ru>
2022-05-18 20:57:43 +0300

    game bulls and cows added the function of choosing a game - single player or with a computer  / seventh day

--- lesson_006/python_snippets/_01_mastermined.py
+++ lesson_006/python_snippets/_01_mastermined.py
@@ -1,4 +1,4 @@
-from mastermind_engine import set_number, check_number, fix_represent, bulls_n_cows
+from mastermind_engine import set_number, check_number, fix_represent, bulls_n_cows, game_celechion
 from termcolor import cprint
 from ii_engine import binary_search
 
@@ -6,19 +6,16 @@ player_response = 0
 reference = set_number(islist=False)
 reference1 = set_number(islist=False)
 move_count = 0
+celect = game_celechion()
 
-
-cprint(" Single player – enter one. Game with computer – enter two. ", on_color='on_green')
-game_selection = input("#### ")
-
-if game_selection == "one" or game_selection == "ONE" or game_selection == "One":
+if celect in ["one", "One", "ONE", "001"]:
     cprint("Try to guess the number puzzled by the computer", color='magenta')
     while player_response != reference:
         player_response = fix_represent(reference=reference)
-        print('*' * 44)
         move_count += 1
-        f = check_number(represented_number=player_response, hidden_number=reference)
+        f = bulls_n_cows(represented_number=player_response, hidden_numbe=reference)
         cprint("bulls {}, cows {}".format(f[0], f[1]), color='red')
+        print('*' * 44)
         if player_response == reference:
             cprint(" You win! Guessed number is {}".format(reference), 'green', attrs=['underline'])
             cprint("Move count {}".format(move_count), 'green', attrs=['underline'])
@@ -30,7 +27,7 @@ if game_selection == "one" or game_selection == "ONE" or game_selection == "One"
             else:
                 cprint("   GAME OVER   ", 'green', 'on_grey', ['reverse'])
 
-elif game_selection == "two" or game_selection == "TWO" or game_selection == "Two":
+elif celect in ["002", "two", "Two", "TWO"]:
     while player_response != reference or computer_response != reference1:
         for i in binary_search(referens=reference1):
             cprint(" Player is playing please enter your number ", color="blue")
@@ -39,23 +36,31 @@ elif game_selection == "two" or game_selection == "TWO" or game_selection == "Tw
             move_count += 1
             f = check_number(represented_number=player_response, hidden_numbe=reference)
             cprint(" bulls {}, cows {}".format(f[0], f[1]), color='red')
-            print('*' * 44)
+
             cprint(" Сomputer is playing ", color="cyan")
 
             computer_response = i
 
             cprint("#### {}".format(computer_response), color= "cyan")
-            f = bulls_n_cows(reference1, computer_response)
+            f = bulls_n_cows(represented_number=reference1, hidden_numbe=computer_response)
             cprint(" bulls {}, cows {}".format(f[0], f[1]), color='red')
+            print('*' * 44)
             if player_response == reference:
-                cprint(" The player won the hidden number {} ".format(reference), on_color='on_blue')
+                cprint(" The player won for the {} moves, hidden number {} ".format(move_count, reference), on_color='on_blue')
+                for i in range(3):
+                    print()
                 question = input("If you want to play more, enter yes, otherwise no ")
+
                 if question == "yes" or question == "YES" or question == "Yes":
                     player_response = "0"
                 else:
                     cprint("   GAME OVER   ", 'green', 'on_grey', ['reverse'])
             elif computer_response == reference1:
-                cprint(" The computer won the guessed number of the player ".format(reference), on_color='on_cyan')
+                cprint(" Компьютер выиграл за {} ходов, скрытое число игрока {} ".format(move_count, reference), on_color='on_cyan')
+                for i in range(3):
+                    print()
+                question = input("If you want to play more, enter yes, otherwise no ")
+
                 if question == "yes" or question == "YES" or question == "Yes":
                     player_response = "0"
                 else:
--- lesson_006/python_snippets/mastermind_engine.py
+++ lesson_006/python_snippets/mastermind_engine.py
@@ -1,9 +1,6 @@
-from random import randint, random, choice
-from itertools import product, count
-
+from random import choice
+from itertools import product
 from termcolor import cprint
-
-_guess_number = 0
 
 everything = " "
 
@@ -18,6 +15,11 @@ def set_number(islist=True):
         return choice(everything)
 
 
+def hidden_number():
+    f = set_number(islist=False)
+    return f
+
+
 def check_number(represented_number, hidden_numbe):
     bulls = 0
     cows = 0
@@ -26,27 +28,22 @@ def check_number(represented_number, hidden_numbe):
             if ip == ir and indexp == indexr:
                 bulls += 1
                 break
-            elif ip == ir and ip != hidden_numbe[indexp]:
+            elif ip == ir and indexp != indexr:
                 cows += 1
-    f = bulls, cows
+    cows_end = cows - bulls
+    f = bulls, cows_end
     return f
 
 
-def hidden_number():
-    f = set_number(islist=False)
-    return f
-
-
-def bulls_n_cows(a, b):
+def bulls_n_cows(represented_number, hidden_numbe):
     set_number(islist=True)
-    assert a in everything and b in everything
-    bulls = sum(1 for x, y in zip(a, b) if x == y)
-    cows = len(set(a) & set(b)) - bulls
+    # assert represented_number in everything and hidden_numbe in everything, " number must be unique digits, try again "
+    bulls = sum(1 for x, y in zip(represented_number, hidden_numbe) if x == y)
+    cows = len(set(represented_number) & set(hidden_numbe)) - bulls
     return bulls, cows
 
 
 def fix_represent(reference):
-
     f = "0"
     while len(f) != str(len(reference)):
         f = input("#### ")
@@ -54,3 +51,20 @@ def fix_represent(reference):
             cprint("invalid input - Enter a four-digit number ", on_color='on_red')
         elif len(f) == len(reference):
             return f
+
+
+def game_celechion():
+    cprint(" Single player – enter one or 1. Game with computer – enter two or 2. ", 'green', attrs=['reverse'])
+    answer = " "
+    answer_list = ["one", "One", "ONE", "1", "2", "two", "Two", "TWO"]
+    while len(answer) != len('one'):
+        answer = input("#### ")
+        if answer not in answer_list:
+            answer = " "
+            cprint(" Please enter the correct answer ", 'green', attrs=['reverse'])
+        elif answer == "1" or answer == "2":
+            answer = "00" + answer
+    return answer
+
+
+
