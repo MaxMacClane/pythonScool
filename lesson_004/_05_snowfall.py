a009b167b2a768d116b7496af853d0a570b00710
maximlarin <Maximlarin@mail.ru>
2022-05-25 23:53:21 +0300

    added a choice of snowflake colors and a function of multi-colored snowflakes

--- lesson_004/_05_snowfall.py
+++ lesson_004/_05_snowfall.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import simple_draw as sd
 import random
-random.seed(120)
+
 sd.resolution = 1000, 600
 # import lesson_006.users_module as use
 # На основе кода из практической части реализовать снегопад:
@@ -21,27 +21,32 @@ sd.resolution = 1000, 600
 
 
 # function return dict wiht list
-def snowf_value():
-    N = 2
+def snowf_value(amount):
+    N = amount
     snowf_lists = []
+    color_list_tuple = [(255, 255, 255), (0, 0, 0), (255, 0, 0),
+                        (255, 127, 0), (255, 255, 0), (0, 255, 0),
+                        (0, 255, 255), (0, 0, 255), (255, 0, 255),
+                        ]
     for i in range(N):
         length_list = [x for x in range(4, 30, random.randint(3, 9))]
         f = {'length': length_list,
              'x': random.randint(0, 1000),
-             'y': random.randint(650, 660)
+             'y': random.randint(650, 660),
+             "color" : random.choice(color_list_tuple)
              }
         snowf_lists.append(f)
     return snowf_lists
 
 
 
-
-
+c = 0
 # infinite loop conditions
-def snowfleaks(color_list):
+def snowfleaks(color_list, amount):
     x, y = 0, 0
-    snowf_lists = snowf_value()
+    snowf_lists = snowf_value(amount=amount, color=True)
     while True:
+
         sd.start_drawing()
         # sd.clear_screen()
         # the loop takes data from the list, draws a white snowflake and a blue circle
@@ -49,6 +54,7 @@ def snowfleaks(color_list):
             for snowf_list in snowf_lists[index]['length']:
                 sd.snowflake(center=sd.get_point(snowf_lists[index]['x'], snowf_lists[index]['y']), length=snowf_list,
                              color=color_list)
+
             sd.circle(center_position=sd.get_point(snowf_lists[index]['x'], snowf_lists[index]['y']), radius=30,
                       color=sd.background_color, width=0)
             snowf_lists[index]['x'] -= sd.random_number(-20, 20)
@@ -65,9 +71,9 @@ def snowfleaks(color_list):
 
         x += 1
         # condition for updating the list with data and continuing the snowfall
-        if x % 2 == 0:
-            for _ in range(1):
-                s = snowf_value()
+        if x % 3 == 0:
+            for _ in range(2):
+                s = snowf_value(amount=amount, color=True)
                 snowf_lists += s
 
             for _ in range(len(snowf_lists)):
@@ -81,9 +87,50 @@ def snowfleaks(color_list):
         if sd.user_want_exit():
             break
 
+def snowfleaks_multicolor( amount):
+    x, y = 0, 0
+    snowf_lists = snowf_value(amount=amount)
+    while True:
+        sd.start_drawing()
+        # sd.clear_screen()
+        # the loop takes data from the list, draws a white snowflake and a blue circle
+        for index, count in enumerate(snowf_lists):
+            for snowf_list in snowf_lists[index]['length']:
+                sd.snowflake(center=sd.get_point(snowf_lists[index]['x'], snowf_lists[index]['y']), length=snowf_list,
+                             color=snowf_lists[index]['color'])
 
-sd.pause()
+            sd.circle(center_position=sd.get_point(snowf_lists[index]['x'], snowf_lists[index]['y']), radius=30,
+                      color=sd.background_color, width=0)
+            snowf_lists[index]['x'] -= sd.random_number(-20, 20)
+            snowf_lists[index]['y'] -= sd.random_number(10, 20)
+        for index, count in enumerate(snowf_lists):
+            for snowf_list in snowf_lists[index]['length']:
+                sd.snowflake(center=sd.get_point(snowf_lists[index]['x'], snowf_lists[index]['y']), length=snowf_list,
+                             color=snowf_lists[index]['color'])
+            # list deletion condition
+            if snowf_lists[index]['y'] < sd.random_number(15, 25):
+                snowf_lists.remove(count)
 
+                # del snowf_lists[index +1]
+                # del snowf_lists[index +2]
+
+        x += 1
+        #condition for updating the list with data and continuing the snowfall
+        if x % 3 == 0:
+            for _ in range(2):
+                s = snowf_value(amount=amount)
+                snowf_lists += s
+
+            for _ in range(len(snowf_lists)):
+                snowf_lists[_]['length'].clear()
+                for length in range(1, sd.random_number(8, 30), 4):
+                    # for length in range(sd.random_number(0, 600)):
+                    snowf_lists[_]['length'].append(length)
+
+        sd.finish_drawing()
+        sd.sleep(0.1)
+        if sd.user_want_exit():
+            break
 # подсказка! для ускорения отрисовки можно
 #  - убрать clear_screen()
 #  - в начале рисования всех снежинок вызвать sd.start_drawing()
--- lesson_006/users_module.py
+++ lesson_006/users_module.py
@@ -1,15 +1,32 @@
 import termcolor as ter
-import simple_draw
+import simple_draw as sd
+from lesson_004 import _05_snowfall as snow
 
 ter.cprint("  Seleck color snowflakes  ", 'blue', attrs=['reverse'])
-color_list_select = {1: "COLOR_WHITE ", 2: "COLOR_BLACK ", 3: "COLOR_RED   ", 4: "COLOR_ORANGE", 5: "COLOR_YELLOW",
-                     6: "COLOR_GREEN ", 7: "COLOR_CYAN  ", 8: "COLOR_BLUE  ", 9: "COLOR_PURPLE", }
-color_list_tuple = {1: (255, 255, 255), 2: (0, 0, 0), 3: (255, 0, 0), 4: (255, 127, 0), 5: (255, 255, 0),
-                    6: (0, 255, 0), 7: (0, 255, 255), 8: (0, 0, 255), 9: (255, 0, 255), }
+
+color_list_select = {"1": "COLOR_WHITE ", "2": "COLOR_BLACK ", "3": "COLOR_RED   ",
+                     "4": "COLOR_ORANGE", "5": "COLOR_YELLOW", "6": "COLOR_GREEN ",
+                     "7": "COLOR_CYAN  ", "8": "COLOR_BLUE  ", "9": "COLOR_PURPLE",
+                     "0": "MULTICOLOR"}
+
+color_list_tuple = {"1": (255, 255, 255), "2": (0, 0, 0), "3": (255, 0, 0),
+                    "4": (255, 127, 0), "5": (255, 255, 0), "6": (0, 255, 0),
+                    "7": (0, 255, 255), "8": (0, 0, 255), "9": (255, 0, 255),
+                    "0": "0"}
 
 for i in color_list_select:
     ter.cprint(" {}:{} ".format(i, color_list_select[i]), 'yellow', attrs=['blink'])
-color_list = int(input())
-from lesson_004._05_snowfall import snowfleaks
+color_list = 0
+while color_list not in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]:
+    color_list = input()
+    if color_list not in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]:
+        ter.cprint("Please enter a number from 1 to 9")
 
-snowfleaks(color_list=color_list_tuple[color_list])
+if color_list == "0":
+    snow.snowfleaks_multicolor(amount=2)
+else:
+    snow.snowfleaks(color_list=color_list_tuple[color_list], amount=2)
+
+print(color_list_tuple[color_list])
+
+sd.pause()
