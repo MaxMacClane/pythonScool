5264c9599c8ab2f733781222266cbc133f0ada4f
maximlarin <Maximlarin@mail.ru>
2022-05-25 19:11:10 +0300

    smoll chenge

--- lesson_005/village_house/rainbow.py
+++ lesson_005/village_house/rainbow.py
@@ -1,13 +1,18 @@
 import simple_draw as sd
-
+sd.resolution=800, 800
 
 rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
 def rainbow():
-    radius = 1400
-    point = sd.get_point(800, 100)
+    radius = 400
+    point = sd.get_point(400, 400)
     # loop thet sort out rainbow_colors reduces radius and calla function  to draw circles
-    for i in range(len(rainbow_colors)):
+    for i in rainbow_colors:
         radius -= 31
-        sd.circle(point, radius, color=rainbow_colors[i], width=30)
+        sd.circle(point, radius, color=i, width=30)
+
+
+rainbow()
+
+sd.pause()
